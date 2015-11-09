19c5ebcab03cf73e29285aabef51109cd65cdcbb
Arthur Rump <arthurrump@outlook.com>
2015-11-09 20:26:51 +0100

    Add EnvironmentVariables to the Startup configuration

--- src/aCMS/Startup.cs
+++ src/aCMS/Startup.cs
@@ -5,11 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.Configuration;
 
 namespace aCMS
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
