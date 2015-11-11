ab186921720f0d59314b78f5677fca6ebc0ab92c
Arthur Rump <arthurrump@outlook.com>
2015-11-11 18:42:36 +0100

    Add new Page model
    Page.cs contains fields for Id, Url, Title and Content. It also has a DateTimeCreated and a
    DateTimeUpdated field, the first one indicates when the page is created, and it should not
    be changed, the second one indicates the last moment changes were made to a page.
    DateTimeDisplay indicates wether or not these dates should be displayed on the view. There's
    also an Author field and an AuthorDisplay. The Author is currently a string, but it will be
    changed to an Author object.

---
+++ src/aCMS/Models/Page.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aCMS.Models
+{
+    public class Page
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; } // Markdown
+
+        [Required]
+        public DateTimeOffset DateTimeCreated { get; set; }
+        [Required]
+        public DateTimeOffset DateTimeUpdated { get; set; }
+        [Required]
+        public bool DateTimeDisplay { get; set; } = false;
+
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public bool AuthorDisplay { get; set; } = false;
+    }
+}
