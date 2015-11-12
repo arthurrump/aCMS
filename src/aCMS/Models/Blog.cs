cbd6bf4ce008850de34a07fe96c8427f1a9b249e
Arthur Rump <arthurrump@outlook.com>
2015-11-12 20:39:14 +0100

    Add Blog model based on ContentBase

---
+++ src/aCMS/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aCMS.Models
+{
+    public class Blog : ContentBase
+    {
+        public Blog()
+        {
+            AuthorDisplay = false;
+            DateTimeDisplay = false;
+        }
+    }
+}
