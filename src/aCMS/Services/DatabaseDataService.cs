c33205a5533ff1c76309a4a6d3f08ed6a51b0cf8
Arthur Rump <arthurrump@outlook.com>
2015-11-13 21:53:27 +0100

    Add DatabaseDataService : IDataService
    The DatabaseDataService<T> implements IDataService and provides CRUD operations for the
    database data. T can be every type that is derived from ContentBase, so it can be used to
    connect to all three tables currently in the database and other future data types derived
    from ContentBase.

---
+++ src/aCMS/Services/DatabaseDataService.cs
@@ -0,0 +1,92 @@
+﻿using aCMS.Models;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Framework.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aCMS.Services
+{
+    public class DatabaseDataService<T> : IDataService<T> where T : ContentBase
+    {
+        private readonly IMemoryCache _cache;
+        private readonly CmsContext _context;
+
+        public DatabaseDataService(IMemoryCache cache, CmsContext context)
+        {
+            _cache = cache;
+            _context = context;
+        }
+
+        public string CacheKey { get; } = nameof(DatabaseDataService<T>);
+
+        public T Add(T data)
+        {
+            _context.Set<T>().Add(data);
+            _context.SaveChanges();
+            RefreshCache();
+
+            return data;
+        }
+
+        public void Delete(T data)
+        {
+            _context.Set<T>().Remove(data);
+            _context.SaveChanges();
+            RefreshCache();
+
+            return;
+        }
+
+        public IEnumerable<T> Get(bool html = true, bool cache = true)
+        {
+            return GetAll(html, cache);
+        }
+
+        public T Get(int id, bool html = true, bool cache = true)
+        {
+            return GetAll(html, cache).Where(x => x.Id == id).Single();
+        }
+
+        public T Update(T data)
+        {
+            _context.Set<T>().Update(data);
+            _context.SaveChanges();
+            RefreshCache();
+
+            return data;
+        }
+
+        private IEnumerable<T> GetAll(bool html, bool cache)
+        {
+            IEnumerable<T> data = null;
+
+            if (cache)
+                data = _cache.Get<IEnumerable<T>>(CacheKey);
+
+            if (data == null)
+            {
+                data = _context.Set<T>().ToList();
+                if (html)
+                    foreach (T d in data)
+                        d.Content = CommonMark.CommonMarkConverter.Convert(d.Content).Trim();
+                data = (from x in data orderby x.DateTimeCreated descending select x);
+                if (cache)
+                    _cache.Set(CacheKey, data);
+            }
+
+            return data;
+        }
+
+        private void RefreshCache()
+        {
+            IEnumerable<T> data = _context.Set<T>();
+            foreach (T d in data)
+                d.Content = CommonMark.CommonMarkConverter.Convert(d.Content).Trim();
+            data = (from x in data orderby x.DateTimeCreated descending select x).ToList();
+            _cache.Set(CacheKey, data);
+        }
+    }
+}
