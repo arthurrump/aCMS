5dcc0448d187556a9134e2464b78899e1fca1139
Arthur Rump <arthurrump@outlook.com>
2015-11-13 20:33:57 +0100

    Add IDataService<T> for data abstraction
    IDataService will provide as the base for all data abstraction layers. It has methods for
    all CRUD operations. T is the type of data the service will provide.

---
+++ src/aCMS/Services/IDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aCMS.Services
+{
+    interface IDataService<T>
+    {
+        string CacheKey { get; }
+
+        IEnumerable<T> Get(bool html = true, bool cache = true);
+        T Get(int id, bool html = true, bool cache = true);
+        T Add(T data);
+        T Update(T data);
+        void Delete(T data);
+    }
+}
