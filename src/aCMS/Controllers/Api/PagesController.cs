f9f1c67ce500035e72f994dea7686d0ddd5fb341
Arthur Rump <arthurrump@outlook.com>
2015-11-14 15:02:09 +0100

    Add api controller PagesController

---
+++ src/aCMS/Controllers/Api/PagesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using aCMS.Models;
+using aCMS.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aCMS.Controllers.Api
+{
+    public class PagesController : DataApiController<Page>
+    {
+        public PagesController(IDataService<Page> dataService)
+        {
+            _dataService = dataService;
+        }
+    }
+}
