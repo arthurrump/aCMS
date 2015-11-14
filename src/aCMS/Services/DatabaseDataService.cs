7bfe4fcc6b073debd05f5b5a60e5a504fcc2a34e
Arthur Rump <arthurrump@outlook.com>
2015-11-14 19:06:14 +0100

    Create a DatabaseBaseService and base DatabaseDataService on it
    All the CRUD operations are now defined in DatabaseBaseService, the GetAll() and
    RefreshCache() methods are implemented in DatabaseDataService. This change will allow a
    Service that doesn't have Content that has to be formatted to html to leverage already
    existing code in DatabaseBaseService.

--- src/aCMS/Services/DatabaseDataService.cs
+++ src/aCMS/Services/DatabaseDataService.cs
@@ -1,20 +1,4 @@
-﻿//     Copyright 2015 Arthur Rump
-//  
-//     Licensed under the Apache License, Version 2.0 (the "License");
-//     you may not use this file except in compliance with the License.
-//     You may obtain a copy of the License at
-//  
-//         http://www.apache.org/licenses/LICENSE-2.0
-//  
-//     Unless required by applicable law or agreed to in writing, software
-//     distributed under the License is distributed on an "AS IS" BASIS,
-//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//     See the License for the specific language governing permissions and
-//     limitations under the License.
-
-using aCMS.Models;
-using Microsoft.AspNet.Mvc.ModelBinding;
-using Microsoft.Data.Entity.Metadata;
+﻿using aCMS.Models;
 using Microsoft.Framework.Caching.Memory;
 using System;
 using System.Collections.Generic;
@@ -23,62 +7,15 @@ using System.Threading.Tasks;
 
 namespace aCMS.Services
 {
-    public class DatabaseDataService<T> : IDataService<T> where T : ContentBase
+    public class DatabaseDataService<T> : DatabaseBaseService<T> where T : ContentBase
     {
-        private readonly IMemoryCache _cache;
-        private readonly CmsContext _context;
-
         public DatabaseDataService(IMemoryCache cache, CmsContext context)
         {
             _cache = cache;
             _context = context;
         }
 
-        public string CacheKey { get; } = $"{nameof(DatabaseDataService<T>)}<{typeof(T)}>";
-
-        public T Add(T data)
-        {
-            _context.Set<T>().Add(data);
-            _context.SaveChanges();
-            RefreshCache();
-
-            return data;
-        }
-
-        public void Delete(T data)
-        {
-            _context.Set<T>().Remove(data);
-            _context.SaveChanges();
-            RefreshCache();
-
-            return;
-        }
-
-        public IEnumerable<T> Get(bool html = true, bool cache = true)
-        {
-            return GetAll(html, cache);
-        }
-
-        public T Get(string url, bool html = true, bool cache = true)
-        {
-            return GetAll(html, cache).Where(x => x.Url == url).Single();
-        }
-
-        public T Get(int id, bool html = true, bool cache = true)
-        {
-            return GetAll(html, cache).Where(x => x.Id == id).Single();
-        }
-
-        public T Update(T data)
-        {
-            _context.Set<T>().Update(data);
-            _context.SaveChanges();
-            RefreshCache();
-
-            return data;
-        }
-
-        private IEnumerable<T> GetAll(bool html, bool cache)
+        protected override IEnumerable<T> GetAll(bool html, bool cache)
         {
             IEnumerable<T> data = null;
 
@@ -99,7 +36,7 @@ namespace aCMS.Services
             return data;
         }
 
-        private void RefreshCache()
+        protected override void RefreshCache()
         {
             IEnumerable<T> data = _context.Set<T>();
             foreach (T d in data)
