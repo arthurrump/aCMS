73732f2d72a9e73f3837e77e22eac1ae4cace213
Arthur Rump <arthurrump@outlook.com>
2015-11-14 14:47:53 +0100

    Create abstract base DataApiController<T> and base BlogsController on it
    DataApiController<T> (where T is based on ContentBase) defines all CRUD operations for
    Blogs, Posts, Pages and other types of ContentBase. Controllers for these datatypes will be
    based on this Controller and only need to implement the constructor for dependency
    injection.

---
+++ src/aCMS/Controllers/Api/DataApiController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using aCMS.Services;
+using Microsoft.Framework.Caching.Memory;
+using aCMS.Models;
+
+namespace aCMS.Controllers.Api
+{
+    /// <summary>
+    /// Generic api controller for data.
+    /// </summary>
+    [Route("api/[controller]")]
+    public abstract class DataApiController<T> : Controller where T : ContentBase
+    {
+        protected IDataService<T> _dataService { get; set; } // Get from di in implementation
+
+        // GET: api/Ts
+        [HttpGet]
+        public IEnumerable<T> Get()
+        {
+            return _dataService.Get(html: false, cache: false);
+        }
+
+        // GET api/Ts/5
+        [HttpGet("{id}")]
+        public T Get(int id)
+        {
+            return _dataService.Get(id, html: false, cache: false);
+        }
+
+        // POST api/Ts
+        [HttpPost]
+        public void Post([FromBody]T data)
+        {
+            if (!ModelState.IsValid || data == null)
+                return;
+
+            _dataService.Add(data);
+        }
+
+        // PUT api/Ts/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]T data)
+        {
+            if (!ModelState.IsValid || data == null || data.Id != id)
+                return;
+
+            if (_dataService.Get(id, html: false, cache: true) == null)
+                return;
+
+            _dataService.Update(data);
+        }
+
+        // DELETE api/Ts/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _dataService.Delete(_dataService.Get(id, html: false, cache: true));
+        }
+    }
+}
