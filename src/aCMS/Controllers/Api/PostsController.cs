132d75b9e7357951678ca26b23365bb4eb66d914
Arthur Rump <arthurrump@outlook.com>
2015-11-14 15:00:29 +0100

    Add api controller PostsController

---
+++ src/aCMS/Controllers/Api/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using aCMS.Models;
+using aCMS.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aCMS.Controllers.Api
+{
+    public class PostsController : DataApiController<Post>
+    {
+        public PostsController(IDataService<Post> dataService)
+        {
+            _dataService = dataService;
+        }
+    }
+}
