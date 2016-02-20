dbfb13c081854927e3f0725b5029006e6df71a92
Arthur Rump <arthurrump@outlook.com>
2016-02-20 17:36:04 +0100

    Revert "Add some stuff for logging in"
    This reverts commit fe46e1bab7af57d3e8aa936c389edb86d830e169.

--- src/aCMS/Startup.cs
+++ src/aCMS/Startup.cs
@@ -51,8 +51,7 @@ namespace aCMS
         {
             services.AddEntityFramework()
                 .AddSqlite()
-                .AddDbContext<CmsContext>(options => options.UseSqlite($"Data Source={_appEnv.ApplicationBasePath}/Data/data.db"))
-                .AddDbContext<UserContext>(options => options.UseSqlite($"Data Source={_appEnv.ApplicationBasePath}/Data/users.db"));
+                .AddDbContext<CmsContext>(options => options.UseSqlite($"Data Source={_appEnv.ApplicationBasePath}/Data/data.db"));
 
             // Add services to connect to the database for each type of data in the database
             services.AddScoped<IDataService<Blog>, DatabaseDataService<Blog>>();
@@ -60,13 +59,11 @@ namespace aCMS
             services.AddScoped<IDataService<Page>, DatabaseDataService<Page>>();
             services.AddScoped<IDataService<Author>, DatabaseAuthorService>();
 
-            services.AddScoped<IUserService, UserService>();
-
             services.AddMvc()
                 .AddRazorOptions(options =>
-                {
-                    options.FileProvider = new PhysicalFileProvider($"{_appEnv.ApplicationBasePath}/Themes/CleanBlog");
-                });
+                    {
+                        options.FileProvider = new PhysicalFileProvider($"{_appEnv.ApplicationBasePath}/Themes/CleanBlog");
+                    });
 
             services.AddInstance(typeof(IConfiguration), Configuration);
         }
