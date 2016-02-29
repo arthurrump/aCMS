4734aeeabefdc8d5537d20c3fcb99b3131d3518e
Arthur Rump <arthurrump@outlook.com>
2016-02-29 20:14:35 +0100

    Update Copyright notice to 2016

--- src/aCMS.Web/Controllers/IndexController.cs
+++ src/aCMS.Web/Controllers/IndexController.cs
@@ -1,4 +1,4 @@
-﻿//     Copyright 2015 Arthur Rump
+﻿//     Copyright 2016 Arthur Rump
 //  
 //     Licensed under the Apache License, Version 2.0 (the "License");
 //     you may not use this file except in compliance with the License.
